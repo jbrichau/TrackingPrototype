91d05a66d5db2a986a7aaf0771a74a5eac3f302f
Johan Brichau <johan@yesplan.be>
2018-12-09 17:54:33 +0100

    a bit of cleanup and getting the LTE to work again

--- Device/lib/microjwt.py
+++ Device/lib/microjwt.py
@@ -4,13 +4,13 @@ from ubinascii import b2a_base64
 from hashlib import sha256
 import rsa
 
-headerfields = { 'typ': 'JWT', 'alg': 'RS256' }
-
 def b42_urlsafe_encode(payload):
     return string.translate(b2a_base64(payload)[:-1].decode('utf-8'),{ ord('+'):'-', ord('/'):'_' })
 
-def encode(payload,private_key):
+def encode(payload,private_key,algorithm):
+    headerfields = { 'typ': 'JWT', 'alg': algorithm }
     content = b42_urlsafe_encode(ujson.dumps(headerfields).encode('utf-8'))
     content = content + '.' + b42_urlsafe_encode(ujson.dumps(payload).encode('utf-8'))
+    # TODO: algorithm selection (now assumes RS256)
     signature = b42_urlsafe_encode(rsa.sign(content,private_key,'SHA-256'))
     return content + '.' + signature
--- Device/main.py
+++ Device/main.py
@@ -31,6 +31,7 @@ wlan = None
 py = Pytrack()
 l76 = L76GNSS(py, timeout=30)
 acc = LIS2HH12()
+TOKEN_VALIDITY = 3600
 
 def led_Breathe():
     if not wlan == None:
@@ -48,15 +49,17 @@ def led_Breathe():
     pycom.rgbled(0x000000)
 
 # Returns a network.LTE object with an active Internet connection.
+# Always first update the gpy to latest modem firmware
+# https://docs.pycom.io/tutorials/lte/firmware.html
 def get_LTE():
     global lte
     if lte == None:
         lte = LTE()
     if lte.isconnected():
         return lte
-    lte.reset()
-    lte.send_at_cmd('AT+CGDCONT=1,"IP","nbiot.iot"')
-    lte.attach()
+    # lte.reset()
+    # lte.send_at_cmd('AT+CGDCONT=1,"IP","nbiot.iot"')
+    lte.attach(band=20, apn="nbiot.iot")
     while not lte.isattached():
         print('Attaching...')
         time.sleep(1)
@@ -93,14 +96,21 @@ def get_WLAN():
         return wlan
     nets = wlan.scan()
     for net in nets:
-        if net.ssid == 'Inceptive Airport':
+        if net.ssid == config.WLAN_SSID:
             print('Network found!')
-            wlan.connect(net.ssid, auth=(net.sec, config.LAN_WPA), timeout=5000)
-            while not wlan.isconnected():
+            wlan.connect(net.ssid, auth=(net.sec, config.WLAN_WPA), timeout=5000)
+            attempts = 0
+            while (not wlan.isconnected()) and (attempts < 10):
                 print('Connecting...')
+                attempts = attempts + 1
                 time.sleep(1)
-            print('WLAN connection succeeded!')
-            return True
+            if wlan.isconnected():
+                print('WLAN connection succeeded')
+                return True
+            else:
+                print('WLAN connection failed')
+                end_WLAN()
+                return False
         else:
             end_WLAN()
             return False
@@ -120,7 +130,8 @@ def end_WLAN():
 # Set the internal real-time clock.
 def set_RTC():
     rtc = machine.RTC()
-    rtc.ntp_sync("pool.ntp.org")
+    # dns lookup on nb-iot does not work...
+    # rtc.ntp_sync("pool.ntp.org")
     utime.sleep_ms(750)
     print('\nRTC Set from NTP to UTC:', rtc.now())
     utime.timezone(3600)
@@ -130,43 +141,33 @@ def b42_urlsafe_encode(payload):
     return string.translate(b2a_base64(payload)[:-1].decode('utf-8'),{ ord('+'):'-', ord('/'):'_' })
 
 def create_jwt(project_id, private_key, algorithm):
-    """Creates a JWT (https://jwt.io) to establish an MQTT connection.
-        Args:
-         project_id: The cloud project ID this device belongs to
-         private_key_file: A path to a file containing either an RSA256 or
-                 ES256 private key.
-         algorithm: The encryption algorithm to use. Either 'RS256' or 'ES256'
-        Returns:
-            An MQTT generated from the given project_id and private key, which
-            expires in 20 minutes. After 20 minutes, your client will be
-            disconnected, and a new JWT will have to be generated.
-        Raises:
-            ValueError: If the private_key_file does not contain a known key.
-        """
-
     token = {
             # The time that the token was issued at
             'iat': utime.time(),
             # The time the token expires.
-            'exp': utime.time() + 60*60,
+            'exp': utime.time() + TOKEN_VALIDITY,
             # The audience field should always be set to the GCP project id.
             'aud': project_id
     }
-
-    # Read the private key file.
-    # with open(private_key_file, 'r') as f:
-    #    private_key = f.read()
     print('Creating JWT with token {}'.format(token))
+    return jwt.encode(token, private_key, algorithm)
 
-    return jwt.encode(token, private_key)
+# def get_mqtt_client(project_id, cloud_region, registry_id, device_id, private_key):
+#     """Create our MQTT client. The client_id is a unique string that identifies
+#     this device. For Google Cloud IoT Core, it must be in the format below."""
+#     client_id = 'projects/{}/locations/{}/registries/{}/devices/{}'.format(project_id, cloud_region, registry_id, device_id)
+#     password = create_jwt(project_id, private_key, 'RS256')
+#     print('Sending message with password {}'.format(password))
+#     return MQTTClient(client_id,'mqtt.googleapis.com',8883,user='ignored',password=password,ssl=True)
 
-def get_mqtt_client(project_id, cloud_region, registry_id, device_id, private_key):
-    """Create our MQTT client. The client_id is a unique string that identifies
-    this device. For Google Cloud IoT Core, it must be in the format below."""
-    client_id = 'projects/{}/locations/{}/registries/{}/devices/{}'.format(project_id,cloud_region,registry_id,device_id)
-    password = create_jwt(project_id, private_key, 'RS256')
-    print('Sending message with password {}'.format(password))
-    return MQTTClient(client_id,'mqtt.googleapis.com',8883,user='ignored',password=password,ssl=True)
+def send_http_payload(project_id, cloud_region, registry_id, device_id, private_key, payload):
+    jwt = create_jwt(project_id, private_key, 'RS256')
+    url = 'https://cloudiotdevice.googleapis.com/v1/projects/{}/locations/{}/registries/{}/devices/{}:publishEvent'.format(project_id, cloud_region, registry_id, device_id)
+    payload = '{}/{}/payload-{}'.format(registry_id, device_id, payload)
+    data = { 'binary_data': b42_urlsafe_encode(payload) }
+    request = requests.post(url, json=data, headers={"authorization": "Bearer {}".format(jwt), "cache-control": "no-cache"})
+    print(request.text)
+    request.close()
 
 # sd = SD()
 # os.mount(sd, '/sd')
@@ -174,24 +175,23 @@ def get_mqtt_client(project_id, cloud_region, registry_id, device_id, private_ke
 
 # main program
 try:
-    if get_WLAN() == None:
-        get_LTE()
+    # get_WLAN()
+    get_LTE()
+
     set_RTC()
     coord = l76.position()
     pitch = acc.pitch() 
     roll = acc.roll() 
 
     led_Breathe()
-
-    jwt = create_jwt("smartrise-221512", config.private_key, 'RS256')
-    url = 'https://cloudiotdevice.googleapis.com/v1/projects/{}/locations/{}/registries/{}/devices/{}:publishEvent'.format(config.project,region,registry,device)
-    payload = '{}/{}/payload-{}'.format('smartrise', 'pycom-gpy', "{} - {} - {},{}".format(coord, machine.RTC().now(), pitch, roll))
-    print(payload)
-    data = { 'binary_data': b42_urlsafe_encode(payload) }
-    request = requests.post(url, json=data, headers={"authorization": "Bearer {}".format(jwt), "cache-control": "no-cache"})
-    print(request.text)
-    request.close()
+    send_http_payload(config.project_id, config.cloud_region, config.registry_id, config.device_id, config.private_key,
+                      "{} - {} - {},{}".format(coord, machine.RTC().now(), pitch, roll))
 
 finally:
-    end_LTE()
-    end_WLAN()
+    if not lte == None:
+        end_LTE()
+    if not wlan == None:
+        end_WLAN()
+
+# while (True):
+#     led_Breathe()
